d43ea564e7806b659e154b893064370e0083454a
ArtVVB <arthur.brown@digilent.com>
2019-08-19 10:57:09 -0700

    Change to two camera system

--- sdk/appsrc/ZedBoard_FMC_Pcam_Adapter_DEMO/main.cc
+++ sdk/appsrc/ZedBoard_FMC_Pcam_Adapter_DEMO/main.cc
@@ -19,8 +19,10 @@
 #include "video/VideoOutput.h"
 #include "video/AXI_VDMA.h"
 #include "video/Scaler.h"
-#include "MIPI_D_PHY_RX.h"
 #include "MIPI_CSI_2_RX.h"
+#include "MIPI_D_PHY_RX.h"
+
+#include <stdint.h>
 
 /* Hardware profile */
 #define IRPT_CTL_DEVID 			XPAR_PS7_SCUGIC_0_DEVICE_ID
@@ -50,15 +52,14 @@
 
 /* *************** */
 uintptr_t constexpr frame_baseaddr 	= 0x0A000000U; //Must make sure linker reserves the region above this
+uintptr_t const porta_offset 		= 0;
 uintptr_t const portb_offset 		= 1920/2*3;
-uintptr_t const portc_offset 		= 1920*1080/2*3;
-uintptr_t const portd_offset 		= (1920*1080/2+1920/2)*3;
 /* *************** */
 
 using namespace digilent;
 
 
-static void input_pipeline_mode_change(AXI_VDMA<ScuGicInterruptController>& vdma_driver, OV5640& cam, Scaler& scaler, Resolution HW_ScaledCaptureRes, Resolution VideoOutputRes, OV5640_cfg::mode_t mode, uintptr_t dphy_baseaddr, uintptr_t csi2_baseaddr, uintptr_t gamma_baseaddr );
+static void input_pipeline_mode_change(AXI_VDMA<ScuGicInterruptController>& vdma_driver, OV5640& cam, Scaler& scaler, Resolution VideoOutputRes, OV5640_cfg::mode_t mode, uintptr_t dphy_baseaddr, uintptr_t csi2_baseaddr, uintptr_t gamma_baseaddr );
 static void output_pipeline_mode_change(AXI_VDMA<ScuGicInterruptController>& vdma_driver, VideoOutput& vid, Resolution VideoOutputRes,u8 master_select);
 
 
@@ -67,7 +68,7 @@ int main()
 {
 	//Init CPU, UART, caches etc.
 	init_platform();
-
+#define _DEBUG
 #ifdef _DEBUG
 	SET_VERBOSE_FLAG();
 #endif
@@ -107,22 +108,6 @@ int main()
 		std::unique_ptr<TCA9546> muxch_b_ptr;
 		std::unique_ptr<OV5640> cam_b_ptr;
 #endif
-#if XPAR_MIPI_D_PHY_RX_NUM_INSTANCES >= 3
-		AXI_VDMA<ScuGicInterruptController> vdma_c_driver(VDMA_C_DEVID, frame_baseaddr+portc_offset, irpt_ctl,
-					0,
-					VDMA_C_S2MM_IRPT_ID);
-		Scaler scaler_c(SCALER_C_DEVID);
-		std::unique_ptr<TCA9546> muxch_c_ptr;
-		std::unique_ptr<OV5640> cam_c_ptr;
-#endif
-#if XPAR_MIPI_D_PHY_RX_NUM_INSTANCES >= 4
-		AXI_VDMA<ScuGicInterruptController> vdma_d_driver(VDMA_D_DEVID, frame_baseaddr+portd_offset, irpt_ctl,
-					0,
-					VDMA_D_S2MM_IRPT_ID);
-		Scaler scaler_d(SCALER_D_DEVID);
-		std::unique_ptr<TCA9546> muxch_d_ptr;
-		std::unique_ptr<OV5640> cam_d_ptr;
-#endif
 
 		//Init of system board finished
 		VERBOSE("FMC Pcam Adapter Rev.demo \r\n");
@@ -139,6 +124,7 @@ int main()
 		{
 			muxch_a_ptr = std::make_unique<TCA9546>(iic_driver, 0, 1 << 0);
 			cam_a_ptr = std::make_unique<OV5640>(*muxch_a_ptr, nopgpio);
+			VERBOSE("Camera on port A initialized\r\n");
 		} catch (std::runtime_error const& e)
 		{
 			VERBOSE("Camera on port A did not initialize correctly: %s", e.what());
@@ -151,33 +137,16 @@ int main()
 		{
 			muxch_b_ptr = std::make_unique<TCA9546>(iic_driver, 0, 1 << 1);
 			cam_b_ptr = std::make_unique<OV5640>(*muxch_b_ptr, nopgpio);
+			VERBOSE("Camera on port B initialized\r\n");
 		} catch (std::runtime_error const& e)
 		{
 			VERBOSE("Camera on port B did not initialize correctly: %s", e.what());
 		}
-		try
-		{
-			muxch_c_ptr = std::make_unique<TCA9546>(iic_driver, 0, 1 << 2);
-			cam_c_ptr = std::make_unique<OV5640>(*muxch_c_ptr, nopgpio);
-		} catch (std::runtime_error const& e)
-		{
-			VERBOSE("Camera on port C did not initialize correctly: %s", e.what());
-		}
-		try
-		{
-			muxch_d_ptr = std::make_unique<TCA9546>(iic_driver, 0, 1 << 3);
-			cam_d_ptr = std::make_unique<OV5640>(*muxch_d_ptr, nopgpio);
-		} catch (std::runtime_error const& e)
-		{
-			VERBOSE("Camera on port D did not initialize correctly: %s", e.what());
-		}
 
 		if(cam_a_ptr){
 			try{
-
-
-		     	input_pipeline_mode_change(vdma_a_driver, *cam_a_ptr, scaler_a, Resolution::R960_540_60_PP, Resolution::R1920_1080_60_PP, OV5640_cfg::mode_t::MODE_1080P_1920_1080_30fps_336M_MIPI, XPAR_MIPI_D_PHY_RX_A_S_AXI_LITE_BASEADDR, XPAR_MIPI_CSI_2_RX_A_S_AXI_LITE_BASEADDR, XPAR_AXI_GAMMACORRECTION_A_BASEADDR);
-
+		     	input_pipeline_mode_change(vdma_a_driver, *cam_a_ptr, scaler_a, Resolution::R1920_1080_60_PP, OV5640_cfg::mode_t::MODE_HALFX_1080P_1920_1080_30fps_336M_MIPI, XPAR_MIPI_D_PHY_RX_A_S_AXI_LITE_BASEADDR, XPAR_MIPI_CSI_2_RX_A_S_AXI_LITE_BASEADDR, XPAR_AXI_GAMMACORRECTION_A_BASEADDR);
+		     	VERBOSE("Input pipeline for cam_a initialized\r\n");
 			}
 			catch(std::exception const& e){
 				VERBOSE("An exception occurred in Input pipeline for cam_a: %s", e.what());
@@ -186,28 +155,11 @@ int main()
 		}
 		 if(cam_b_ptr){
 			try{
-		    input_pipeline_mode_change(vdma_b_driver, *cam_b_ptr, scaler_b, Resolution::R960_540_60_PP, Resolution::R1920_1080_60_PP, OV5640_cfg::mode_t::MODE_1080P_1920_1080_30fps_336M_MIPI, XPAR_MIPI_D_PHY_RX_B_S_AXI_LITE_BASEADDR, XPAR_MIPI_CSI_2_RX_B_S_AXI_LITE_BASEADDR, XPAR_AXI_GAMMACORRECTION_B_BASEADDR);
+				input_pipeline_mode_change(vdma_b_driver, *cam_b_ptr, scaler_b, Resolution::R1920_1080_60_PP, OV5640_cfg::mode_t::MODE_HALFX_1080P_1920_1080_30fps_336M_MIPI, XPAR_MIPI_D_PHY_RX_B_S_AXI_LITE_BASEADDR, XPAR_MIPI_CSI_2_RX_B_S_AXI_LITE_BASEADDR, XPAR_AXI_GAMMACORRECTION_B_BASEADDR);
+				VERBOSE("Input pipeline for cam_b initialized\r\n");
 			}
 			catch(std::exception const& e){
 				VERBOSE("An exception occurred in Input pipeline for cam_b: %s", e.what());
-			}
-		}
-		 if(cam_c_ptr){
-			try{
-				input_pipeline_mode_change(vdma_c_driver, *cam_c_ptr, scaler_c, Resolution::R960_540_60_PP, Resolution::R1920_1080_60_PP, OV5640_cfg::mode_t::MODE_1080P_1920_1080_30fps_336M_MIPI, XPAR_MIPI_D_PHY_RX_C_S_AXI_LITE_BASEADDR, XPAR_MIPI_CSI_2_RX_C_S_AXI_LITE_BASEADDR, XPAR_AXI_GAMMACORRECTION_C_BASEADDR);
-
-			}
-			catch(std::exception const& e){
-				VERBOSE("An exception occurred in Input pipeline for cam_c: %s", e.what());
-			}
-		}
-		 if(cam_d_ptr){
-			try{
-				input_pipeline_mode_change(vdma_d_driver, *cam_d_ptr, scaler_d, Resolution::R960_540_60_PP, Resolution::R1920_1080_60_PP, OV5640_cfg::mode_t::MODE_1080P_1920_1080_30fps_336M_MIPI, XPAR_MIPI_D_PHY_RX_D_S_AXI_LITE_BASEADDR, XPAR_MIPI_CSI_2_RX_D_S_AXI_LITE_BASEADDR, XPAR_AXI_GAMMACORRECTION_D_BASEADDR);
-
-			}
-			catch(std::exception const& e){
-				VERBOSE("An exception occurred in Input pipeline for cam_d: %s", e.what());
 			}
 		}
 
@@ -216,12 +168,6 @@ int main()
 		}
 			else if(cam_b_ptr){
 				read_master_select  = 0x01;
-		}
-			else if(cam_c_ptr){
-				read_master_select  = 0x02;
-		}
-			else if(cam_d_ptr){
-				read_master_select  = 0x03;
 		}
 
 		output_pipeline_mode_change(vdma_a_driver, vid, Resolution::R1920_1080_60_PP,read_master_select);
@@ -237,13 +183,16 @@ int main()
 		VERBOSE("Exception that could not be handled: %s", e.what());
 
 	}
+
+	while (1);
+
 	cleanup_platform();
 }
 
 // Global function that sets the camera sensor to live image
 
 
-void input_pipeline_mode_change(AXI_VDMA<ScuGicInterruptController>& vdma_driver, OV5640& cam, Scaler& scaler, Resolution HW_ScaledCaptureRes, Resolution VideoOutputRes, OV5640_cfg::mode_t mode, uintptr_t dphy_baseaddr, uintptr_t csi2_baseaddr, uintptr_t gamma_baseaddr )
+void input_pipeline_mode_change(AXI_VDMA<ScuGicInterruptController>& vdma_driver, OV5640& cam, Scaler& scaler, Resolution VideoOutputRes, OV5640_cfg::mode_t mode, uintptr_t dphy_baseaddr, uintptr_t csi2_baseaddr, uintptr_t gamma_baseaddr )
 {
 	//Bring up input pipeline back-to-front
 	{
@@ -254,9 +203,14 @@ void input_pipeline_mode_change(AXI_VDMA<ScuGicInterruptController>& vdma_driver
 	}
 
 	{
-		vdma_driver.configureWrite(timing[static_cast<int>(HW_ScaledCaptureRes)].h_active, timing[static_cast<int>(HW_ScaledCaptureRes)].v_active, timing[static_cast<int>(VideoOutputRes)].h_active, timing[static_cast<int>(VideoOutputRes)].v_active);
+		u32 VDMA_WriteRes_h_active = 960;
+		u32 VDMA_WriteRes_v_active = 1080;
+		u32 HW_ScaledRes_h_active = 960;
+		u32 HW_ScaledRes_v_active = 1080;
+
+		vdma_driver.configureWrite(VDMA_WriteRes_h_active, VDMA_WriteRes_v_active, timing[static_cast<int>(VideoOutputRes)].h_active, timing[static_cast<int>(VideoOutputRes)].v_active);
 		Xil_Out32(gamma_baseaddr, 3); // Set Gamma correction factor to 1/1.8
-		scaler.setStreams(1920, 1080, 960, 540);
+		scaler.setStreams(HW_ScaledRes_h_active, HW_ScaledRes_v_active, VDMA_WriteRes_h_active, VDMA_WriteRes_v_active);
 		//TODO CSI-2, D-PHY config here
 		cam.init();
 	}
